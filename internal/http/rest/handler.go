659ce6db3e46a8f3c0dbf9a08e41c954415558b2
Kelvin Dealca <dealca@fsi.io>
2020-07-06 20:03:44 -0700

    Add http get for a restaurant by id

--- internal/http/rest/handler.go
+++ internal/http/rest/handler.go
@@ -2,7 +2,9 @@ package rest
 
 import (
 	"encoding/json"
+	"fmt"
 	"net/http"
+	"strconv"
 
 	"github.com/julienschmidt/httprouter"
 	"github.com/kelvinatorr/restaurant-tracker/internal/lister"
@@ -13,6 +15,7 @@ func Handler(l lister.Service) http.Handler {
 	router := httprouter.New()
 
 	router.GET("/restaurants", getRestaurants(l))
+	router.GET("/restaurants/:id", getRestaurant(l))
 
 	return router
 }
@@ -23,5 +26,25 @@ func getRestaurants(s lister.Service) func(w http.ResponseWriter, r *http.Reques
 		w.Header().Set("Content-Type", "application/json")
 		list := s.GetRestaurants()
 		json.NewEncoder(w).Encode(list)
+	}
+}
+
+func getRestaurant(s lister.Service) func(w http.ResponseWriter, r *http.Request, p httprouter.Params) {
+	return func(w http.ResponseWriter, r *http.Request, p httprouter.Params) {
+		// get the route parameter
+		ID, err := strconv.Atoi(p.ByName("id"))
+		if err != nil {
+			http.Error(w, fmt.Sprintf("%s is not a valid beer ID, it must be a number.", p.ByName("id")),
+				http.StatusBadRequest)
+			return
+		}
+
+		restaurant, err := s.GetRestaurant(int64(ID))
+		if err != nil {
+			http.Error(w, err.Error(), http.StatusNotFound)
+		} else {
+			w.Header().Set("Content-Type", "application/json")
+			json.NewEncoder(w).Encode(restaurant)
+		}
 	}
 }
