2e1467512d5235f2c2ee4fa2b17a8d30e7a31b5c
Kelvin Dealca <dealca@fsi.io>
2020-07-22 18:00:01 -0700

    Add json struct field tags

--- internal/lister/restaurant.go
+++ internal/lister/restaurant.go
@@ -1,35 +1,35 @@
 package lister
 
 type Restaurant struct {
-	ID         int64
-	Name       string
-	Cuisine    string
-	Note       string
-	Address    string
-	CityState  cityState
-	Zipcode    string
-	Latitude   float32
-	Longitude  float32
-	GmapsPlace gmapsPlace
+	ID         int64      `json:"id"`
+	Name       string     `json:"name"`
+	Cuisine    string     `json:"cuisine"`
+	Note       string     `json:"note"`
+	Address    string     `json:"address"`
+	CityState  cityState  `json:"city_state"`
+	Zipcode    string     `json:"zipcode"`
+	Latitude   float32    `json:"latitude"`
+	Longitude  float32    `json:"longitude"`
+	GmapsPlace gmapsPlace `json:"gmaps_place"`
 }
 
 type cityState struct {
-	ID    int64
-	Name  string
-	State string
+	ID    int64  `json:"id"`
+	Name  string `json:"name"`
+	State string `json:"state"`
 }
 
 type gmapsPlace struct {
-	ID                   int64
-	LastUpdated          string
-	PlaceID              string
-	BusinessStatus       string
-	FormattedPhoneNumber string
-	Name                 string
-	PriceLevel           int
-	Rating               float32
-	URL                  string
-	UserRatingsTotal     int
-	UTCOffset            int
-	Website              string
+	ID                   int64   `json:"id"`
+	LastUpdated          string  `json:"last_updated"`
+	PlaceID              string  `json:"place_id"`
+	BusinessStatus       string  `json:"business_status"`
+	FormattedPhoneNumber string  `json:"formatted_phone_number"`
+	Name                 string  `json:"name"`
+	PriceLevel           int     `json:"price_level"`
+	Rating               float32 `json:"rating"`
+	URL                  string  `json:"url"`
+	UserRatingsTotal     int     `json:"user_ratings_total"`
+	UTCOffset            int     `json:"utc_offset"`
+	Website              string  `json:"website"`
 }
--- internal/updater/restaurant.go
+++ internal/updater/restaurant.go
@@ -1,36 +1,36 @@
 package updater
 
 type Restaurant struct {
-	ID         int64
-	Name       string
-	Cuisine    string
-	Note       string
-	Address    string
-	CityState  CityState
-	Zipcode    string
-	Latitude   float32
-	Longitude  float32
-	GmapsPlace GmapsPlace
-	CityID     int64
+	ID         int64      `json:"id"`
+	Name       string     `json:"name"`
+	Cuisine    string     `json:"cuisine"`
+	Note       string     `json:"note"`
+	Address    string     `json:"address"`
+	CityState  CityState  `json:"city_state"`
+	Zipcode    string     `json:"zipcode"`
+	Latitude   float32    `json:"latitude"`
+	Longitude  float32    `json:"longitude"`
+	GmapsPlace GmapsPlace `json:"gmaps_place"`
+	CityID     int64      `json:"city_id"`
 }
 
 type CityState struct {
-	Name  string
-	State string
+	Name  string `json:"name"`
+	State string `json:"state"`
 }
 
 type GmapsPlace struct {
-	ID                   int64
-	LastUpdated          string
-	PlaceID              string
-	BusinessStatus       string
-	FormattedPhoneNumber string
-	Name                 string
-	PriceLevel           int
-	Rating               float32
-	URL                  string
-	UserRatingsTotal     int
-	UTCOffset            int
-	Website              string
-	RestaurantID         int64
+	ID                   int64   `json:"id"`
+	LastUpdated          string  `json:"last_updated"`
+	PlaceID              string  `json:"place_id"`
+	BusinessStatus       string  `json:"business_status"`
+	FormattedPhoneNumber string  `json:"formatted_phone_number"`
+	Name                 string  `json:"name"`
+	PriceLevel           int     `json:"price_level"`
+	Rating               float32 `json:"rating"`
+	URL                  string  `json:"url"`
+	UserRatingsTotal     int     `json:"user_ratings_total"`
+	UTCOffset            int     `json:"utc_offset"`
+	Website              string  `json:"website"`
+	RestaurantID         int64   `json:"restaurant_id"`
 }
