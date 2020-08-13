c83290a5234163ca0f5547a80768f61aca6b1bfe
Kelvin Dealca <dealca@fsi.io>
2020-08-13 16:01:29 -0700

    Remove json tag for VisitID because we fill this in after we insert

--- internal/adder/visit-user.go
+++ internal/adder/visit-user.go
@@ -1,7 +1,7 @@
 package adder
 
 type VisitUser struct {
-	VisitID int64  `json:"id"`
-	UserID  int64  `json:"user_id"`
+	VisitID int64
+	UserID  int64 `json:"user_id"`
 	Rating  int64 `json:"rating"`
 }
